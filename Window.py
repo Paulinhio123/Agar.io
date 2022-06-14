08679c96939cd47eec72abfd6fc6d3f5bd2564aa
Paulinhio123 <94375262+Paulinhio123@users.noreply.github.com>
2022-06-15 00:32:59 +0200

    Add files via upload

---
+++ Window.py
@@ -0,0 +1,18 @@
+###############################################
+# Créateur : PETIT, HAMADOUCHE                #
+# Date : 08/06/2022                           #
+# Définition de notre classe Window           #
+# Version : V0.1                              #
+###############################################
+import core
+
+class Window :
+
+    def display(self):
+        core.WINDOW_SIZE = [750, 750]
+        core.fps = 144
+        core.bgColor = (255, 255, 255)
+
+    def text(self):
+        pass
+
